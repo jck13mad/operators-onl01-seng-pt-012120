4262001a59ded19aca487e655167aa42644e48d5
Jack Madden <jck13mad@gmail.com>
2020-01-23 21:13:46 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+  if speed < 40
+    return "true"
+  elsif spped > 60
+    return "true"
+  else 
+    return "false"
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 ? "true" : "false"
+	speed > 60 ? "true" : "false" 
 end
 	
 
